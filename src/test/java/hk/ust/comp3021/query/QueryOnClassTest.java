3d14dbb70732e7175ced457c6712102cd991b7b0
BowenZhang-UST <bowen.zhang@connect.ust.hk>
2024-03-31 23:08:25 +0800

    update hidden cases for class query:

--- src/test/java/hk/ust/comp3021/query/QueryOnClassTest.java
+++ src/test/java/hk/ust/comp3021/query/QueryOnClassTest.java
@@ -101,6 +101,89 @@ public class QueryOnClassTest {
         );
     }
 
+    @Tag(TestKind.HIDDEN)
+    @ParameterizedTest
+    @MethodSource("dataHaveSuperClass")
+    public void testHaveSuperClassHidden(String caseID, String FuncA, String FuncB, Boolean expectedOutput) {
+        ASTManagerEngine engine = new ASTManagerEngine();
+        engine.processXMLParsing("resources/pythonxml", caseID);
+        QueryOnClass queryOnClass = new QueryOnClass(engine.getId2ASTModules().get(caseID));
+        Boolean hasSuper = queryOnClass.haveSuperClass.apply(FuncA, FuncB);
+        assertEquals(expectedOutput, hasSuper);
+    }
+
+    private static Stream<Object[]> dataHaveSuperClass() {
+        return Stream.of(
+            new Object[] {"7","Bar", "Baz", true},
+            new Object[] {"7", "Foo", "Bar", false}
+        );
+    }
+
+    @Tag(TestKind.HIDDEN)
+    @ParameterizedTest
+    @MethodSource("dataFindOverrideMethods")
+    public void testFindOverrideMethodsHidden(String caseID, List<String> expectedOutput) {
+        ASTManagerEngine engine = new ASTManagerEngine();
+        engine.processXMLParsing("resources/pythonxml", caseID);
+        QueryOnClass queryOnClass = new QueryOnClass(engine.getId2ASTModules().get(caseID));
+        List<String> superClasses = queryOnClass.findOverridingMethods.get();
+
+        assertEquals(expectedOutput.size(), superClasses.size());
+        for (String superClass : superClasses) {
+            int index = expectedOutput.indexOf(superClass);
+            if(index != -1) {
+                expectedOutput.remove(index);
+            }
+        }
+        assertEquals(expectedOutput.size(), 0);
+    }
+
+    private static Stream<Object[]> dataFindOverrideMethods() {
+        return Stream.of(
+            new Object[] {"8", new ArrayList<>(Arrays.asList("baz", "foo", "foo"))},
+            new Object[] {"9", new ArrayList<>(Arrays.asList())}
+        );
+    }
+
+    @Tag(TestKind.HIDDEN)
+    @ParameterizedTest
+    @MethodSource("dataFindAllMethods")
+    public void testFindAllMethodsHidden(String caseID, String inputFunc, Set<String> expectedOutput) {
+        ASTManagerEngine engine = new ASTManagerEngine();
+        engine.processXMLParsing("resources/pythonxml", caseID);
+        QueryOnClass queryOnClass = new QueryOnClass(engine.getId2ASTModules().get(caseID));
+        List<String> allMethods = queryOnClass.findAllMethods.apply(inputFunc);
+
+        assertEquals(expectedOutput, new HashSet<String>(allMethods));
+    }
+
+    private static Stream<Object[]> dataFindAllMethods() {
+        return Stream.of(
+            new Object[] {"7", "Bar", Set.of("foo", "bar", "baz")},
+            new Object[] {"8", "Bar", Set.of("foo", "bar", "baz")}
+        );
+    }
+
+    @Tag(TestKind.HIDDEN)
+    @ParameterizedTest
+    @MethodSource("dataFindClassesWithMain")
+    public void testFindClassesWithMainHidden(String caseID,  Set<String> expectedOutput) {
+        ASTManagerEngine engine = new ASTManagerEngine();
+        engine.processXMLParsing("resources/pythonxml", caseID);
+        QueryOnClass queryOnClass = new QueryOnClass(engine.getId2ASTModules().get(caseID));
+        List<String> classesWithMain = queryOnClass.findClassesWithMain.get();
+
+        assertEquals(expectedOutput, new HashSet<String>(classesWithMain));
+    }
+
+    private static Stream<Object[]> dataFindClassesWithMain() {
+        return Stream.of(
+            new Object[] {"9", Set.of("Baz", "Bar", "Foo")},
+            new Object[] {"9", Set.of("Baz", "Bar", "Foo")}
+        );
+    }
+
+
 
 
 }
